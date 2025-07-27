5ddc1af36f2e05452e3ff8c7b44b5f6a979a0bac
Javier Mendoza <javier.mendoza@nzxt.com>
2025-07-27 14:01:34 -0700

    add server

---
+++ server.ts
@@ -0,0 +1,21 @@
+import index from "./main/index.html";
+const PORT = 3000;
+// serve /main/index.html
+Bun.serve({
+  port: PORT,
+  hostname: "0.0.0.0",
+  routes: {
+    "/*": index,
+  },
+
+  development: process.env.NODE_ENV !== "production" && {
+    // Enable browser hot reloading in development
+    hmr: true,
+
+    // Echo console logs from the browser to the server
+    console: true,
+  },
+});
+
+console.log(`Server is running on port ${PORT}`);
+console.log(`http://localhost:${PORT}`);
